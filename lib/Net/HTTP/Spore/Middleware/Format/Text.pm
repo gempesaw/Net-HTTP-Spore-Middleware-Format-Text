85ee0d30cff59a12613adb96005e9a29bc842cac
Daniel Gempesaw <gempesaw@gmail.com>
2015-08-30 09:29:59 -0400

    initial commit

---
+++ lib/Net/HTTP/Spore/Middleware/Format/Text.pm
@@ -0,0 +1,15 @@
+package Net::HTTP::Spore::Middleware::Format::Text;
+
+# ABSTRACT:
+use strict;
+use warnings;
+
+=for markdown [![Build Status](https://travis-ci.org/gempesaw/Net-HTTP-Spore-Middleware-Format-Text.svg?branch=master)](https://travis-ci.org/gempesaw/Net-HTTP-Spore-Middleware-Format-Text)
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+1;
